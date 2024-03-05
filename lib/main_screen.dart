fe15750314b1a78b4315d68b3fad7debdcfcd390
Ayaliswa <ayandambuyisa5@gmail.com>
2024-03-05 15:35:58 +0200

    stokvel

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,8 +1,6 @@
 import "package:flutter/material.dart";
 import 'package:stokvel/security/login_screen.dart';
 import 'package:stokvel/registration/sign_up_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
